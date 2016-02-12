26381b927c62c6dd38288a8a32649529e1109052
davood96 <david96@my.yorku.ca>
2016-02-12 15:46:55 -0500

    History works for binary operators (to some extent). Still needs to be tested. From now on, just make updates to this project please. PS: sin, cos and +/- need to be fixed

---
+++ CalcV7/src/Model.java
@@ -0,0 +1,761 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+
+public class Model
+{
+	private Stack<String> button_history = new Stack<String>();
+	private Stack<String> prev_history = new Stack<String>();
+	private Stack<Double> stored_values = new Stack<Double>();
+	private Stack<String> precedence= new Stack<String>();
+	
+	
+	
+	private static final String EQUALS = "=";
+	private static final String START = "Start new Calculation";
+	private static final String ENTER = "Enter";
+	private static final String FACT = "!";
+	private static final String PLUS = "+";
+	private static final String MINUS = "-";
+	private static final String MULT = "x";
+	private static final String DIV = "/";
+	
+	private ArrayList<String> highest_precedence = new ArrayList<String>();
+	private ArrayList<String> lowest_precedence = new ArrayList<String>();
+	
+	private StringBuilder sb = new StringBuilder();
+	private StringBuilder sb_input_history = new StringBuilder();
+	private ArrayList<String> running_history = new ArrayList<String>();
+	private StringBuilder sb_completed_operations = new StringBuilder();
+	
+	private boolean from_memory = false;
+	//private boolean bin_op = false;
+	
+	public Model()
+	{
+		reset();
+		highest_precedence.add(MULT);
+		highest_precedence.add(DIV);
+		lowest_precedence.add(PLUS);
+		lowest_precedence.add(MINUS);
+		
+	}
+	
+	
+	
+	private String printHistory(ArrayList<String> arraylist, int size)
+	{
+		if(size == 1)
+		{
+			return arraylist.get(0);
+		}
+		return printHistory(arraylist, size - 1) + ", " + arraylist.get(size - 1);
+	}
+	
+	
+	public void addToEntry(String button)
+	{
+		sb.append(button);
+		sb_input_history.append(button);
+	}
+	
+	public String updateValue()
+	{
+		return sb.toString();
+	}
+	
+	public void reset()
+	{
+		stored_values.clear();
+		button_history.clear();
+		prev_history.clear();
+		precedence.clear();
+		
+		sb_input_history.delete(0, sb_input_history.length());
+		sb_completed_operations.delete(0, sb_completed_operations.length());
+		running_history.clear();
+		sb.delete(0, sb.length());
+	}
+	
+	public String sum()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = addStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = addStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+	}
+	
+	public String subtract()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = subStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = subStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+	}
+	
+	public String multiply()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = multStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = multStoredWithHistory();
+			sb.delete(0, sb.length());
+			//getNewHistory(button);
+			return "" + value;
+		}
+	}
+	
+	public String divide()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = divStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = divStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+	}
+	
+	public String negate()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = negateStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = negateStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+
+	}
+	
+	public String factorial()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = factStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = factStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		
+	}
+	
+	public String sin()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = sinStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = sinStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+
+	}
+
+	public String cos()
+	{
+		if(sb.toString().equals(""))
+		{
+			from_memory = true;
+			double value = cosStoredValues();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+		else
+		{
+			from_memory = false;
+			double value = cosStoredWithHistory();
+			sb.delete(0, sb.length());
+			return "" + value;
+		}
+
+	}
+	
+	public String operandHistory(String operand)
+	{
+		String first;
+		String second;
+		
+		//Add last action
+		prev_history.push(button_history.peek());
+		
+		
+		second = button_history.pop();
+		
+		if(!from_memory)
+		{
+			//If the stack is empty or operation is completed using a button press, 
+			//use the button press to update to the stack
+			if(!checkBrackets(second, operand))
+			{
+				sb_completed_operations.append(second + " " + operand + " " + sb_input_history.toString() + " ");
+			}
+			else
+			{
+				sb_completed_operations.append("(" + second + ")" + " " + operand + " " +  sb_input_history.toString()  + " ");
+			}
+			
+			//Update precedence list
+			precedence.push(operand);
+			
+			System.out.println(precedence.toString());
+			
+			//Reset button-press string for next use
+			sb_input_history.delete(0, sb_input_history.length());
+			
+			//Add updated history to the stack (First and only element)
+			button_history.push(sb_completed_operations.toString());
+			
+			// Reset completed operations string_builder for next entry
+			sb_completed_operations.delete(0, sb_completed_operations.length());
+			
+			
+			running_history.remove(running_history.size() - 1);
+			running_history.add(button_history.peek());
+			prev_history.push(operand);
+			
+			return printHistory(running_history, running_history.size()) + EQUALS;
+		}
+		//Else, use the last two entries in the stack and anything else as necessary
+		
+		first = button_history.pop();
+		
+		
+		if(checkBrackets(second, operand))	
+		{
+			System.out.println("second is true");
+			if(checkBrackets(first, operand))
+				sb_completed_operations.append("(" + first +  ") " + operand + " " + "(" + second + ")" + " ");
+			else
+			{
+				sb_completed_operations.append(first +  " " + operand + " " + "(" + second + ")" + " ");
+			}
+				
+		}
+		else if(checkBrackets(first, operand))
+		{
+			sb_completed_operations.append("(" + first +  ") " + operand + " " + second + " ");
+		}
+		else
+		{
+			sb_completed_operations.append(first + " " + operand + " " /*+ sb_input_history.toString() + " "*/ + second + " ");
+		}
+		
+		
+		
+		//Update precedence list
+		precedence.push(operand);
+		
+		System.out.println(precedence.toString());
+		
+		// Add updated history to the stack
+		button_history.push(sb_completed_operations.toString());
+		
+		// Reset completed operations string_builder for next use.
+		// This prevents duplication of previous entries
+		sb_completed_operations.delete(0, sb_completed_operations.length());
+		
+		//remove last two elements in running_history
+		for(int i = 0; i < 2; i++)
+		{
+			if(!(running_history.isEmpty()))
+			{
+				running_history.remove(running_history.size() - 1);
+			}
+			
+		}
+		//replace them with updated computation
+		running_history.add(button_history.peek());
+		
+		prev_history.push(operand);
+		//print updated history
+		return printHistory(running_history, running_history.size()) + EQUALS;
+	}
+	
+	public String singleOperandHistory(String operand)
+	{
+		//Add last action
+		//prev_history.push(button_history.peek());
+		
+		if(!from_memory)
+		{
+			//set the string
+			sb_completed_operations.append(sb_input_history.toString() + operand + " ");
+			
+			//Reset button-press string for next use
+			sb_input_history.delete(0, sb_input_history.length());
+			
+			//Add updated history to the stack (First and only element)
+			button_history.push(sb_completed_operations.toString());
+			
+			// Reset completed operations string_builder for next entry
+			sb_completed_operations.delete(0, sb_completed_operations.length());
+			
+			
+				if(!(running_history.isEmpty()))
+				{
+					running_history.remove(running_history.size()- 1);
+				}
+		
+			//replace them with updated computation
+			running_history.add(button_history.peek());
+			prev_history.push(operand);
+			
+			//Update precedence list
+			precedence.push(operand);
+			
+			return printHistory(running_history, running_history.size()) + EQUALS;
+			
+		}
+		else 
+		{
+			String first = button_history.pop();
+			
+			if(!checkBrackets(first, operand))
+				sb_completed_operations.append(first + operand + " ");
+			else
+				sb_completed_operations.append("(" + first + ")" + operand + " ");
+			
+			//Update precedence list
+			precedence.push(operand);
+		
+			sb_input_history.delete(0, sb_input_history.length());
+			// Add updated history to the stack
+			button_history.push(sb_completed_operations.toString());
+			
+			// Reset completed operations string_builder for next use.
+			// This prevents duplication of previous entries
+			sb_completed_operations.delete(0, sb_completed_operations.length());
+		
+			//remove last two elements in running_history
+			for(int i = 0; i < 1; i++)
+			{
+				if(!(running_history.isEmpty()))
+				{
+					running_history.remove(running_history.size() - 1);
+				}
+			
+			}
+			//replace them with updated computation
+			running_history.add(button_history.peek());
+		
+			prev_history.push(operand);
+			
+			//print updated history
+			return printHistory(running_history, running_history.size()) + EQUALS;
+		}
+				
+		
+	}
+	
+	public String enterHistory()
+	{
+			
+			//Update prev_history
+			if(button_history.empty())
+			{
+				//If machine started fresh, push default start message
+				prev_history.push(START);
+			}
+			else
+			{
+				//Else, push last entry
+				prev_history.push(button_history.peek());
+				prev_history.push(ENTER);
+			}
+			
+			//System.out.println(prev_history.toString());
+			
+			button_history.push(sb_input_history.toString());
+			running_history.add(sb_input_history.toString());
+			sb_input_history.delete(0, sb_input_history.length());
+			return printHistory(running_history, running_history.size());
+		
+	}
+	
+	public String undoHistory()
+	{
+		//Remove top elements
+		button_history.pop();
+		running_history.remove(running_history.size() - 1);
+		
+		if(isBinOp())
+		{
+			//If last action i s Binary operation,
+			//replace removed element with
+			//top two elements in previous history
+			
+			String second = prev_history.pop();
+			prev_history.pop();
+			String one = prev_history.pop();
+			
+			button_history.push(one);
+			running_history.add(one);
+			button_history.push(second);
+			running_history.add(second);
+		}
+		else if(isSingleOp())
+		{
+			running_history.add(prev_history.pop());
+		}
+		else
+		{
+			prev_history.pop();
+		}
+		
+		System.out.println(prev_history.toString());
+		
+		return printHistory(running_history, running_history.size());
+		
+		
+		
+	}
+	
+	public String enterValue()
+	{
+		String placeholder = sb.toString();
+		stored_values.push(Double.parseDouble(sb.toString()));
+		sb.delete(0, sb.length());
+		return placeholder;
+	}
+	
+	private double addStoredValues()
+	{
+		double first_number = stored_values.pop();
+		double second_number = stored_values.pop();
+		double result = first_number + second_number;
+		
+		stored_values.push(result);
+		
+		return result;
+	}
+	
+	private double addStoredWithHistory()
+	{
+		double stored = stored_values.pop();
+		double history = Double.parseDouble(sb.toString());
+		double result = stored + history;
+		
+		stored_values.push(result);
+		
+		return result;
+		
+	}
+	
+	private double subStoredValues()
+	{
+		double first_number = stored_values.pop();
+		double second_number = stored_values.pop();
+		double result = second_number - first_number;
+		
+		stored_values.push(result);
+		
+		return result;
+	}
+	
+	private double subStoredWithHistory()
+	{
+		double stored = stored_values.pop();
+		double history = Double.parseDouble(sb.toString());
+		double result = stored - history;
+		
+		stored_values.push(result);
+		
+		return result;
+		
+	}
+	
+	private double multStoredValues()
+	{
+		double first_number = stored_values.pop();
+		double second_number = stored_values.pop();
+		double result = second_number * first_number;
+		
+		stored_values.push(result);
+		
+		return result;
+	}
+	
+	private double multStoredWithHistory()
+	{
+		double stored = stored_values.pop();
+		double history = Double.parseDouble(sb.toString());
+		double result = stored * history;
+		
+		stored_values.push(result);
+		
+		return result;
+		
+	}
+	
+	private double divStoredValues()
+	{
+		double first_number = stored_values.pop();
+		double second_number = stored_values.pop();
+		double result = second_number / first_number;
+		
+		stored_values.push(result);
+		
+		return result;
+	}
+	
+	private double divStoredWithHistory()
+	{
+		double stored = stored_values.pop();
+		double history = Double.parseDouble(sb.toString());
+		double result = stored / history;
+		
+		stored_values.push(result);
+		
+		return result;
+		
+	}
+	
+	private double factStoredValues()
+	{
+		double first_number = stored_values.pop();
+		double result = 1;
+		
+		for(int i = 2; first_number >= i; i++)
+		{
+			result = result * i;
+		}
+		
+		
+		stored_values.push(result);
+		
+		return result;
+	}
+	
+	private double factStoredWithHistory()
+	{
+		double history = Double.parseDouble(sb.toString());
+		double result = 1;
+		
+		for(int i = 1; history >= i; ++i)
+		{
+			result *= i;
+		}
+		
+		stored_values.push(result);
+		
+		return result;
+		
+	}
+	
+	private double sinStoredValues()
+	{
+		//System.out.println("wee");
+		double first_number = stored_values.pop();
+		//System.out.println(second_number);
+
+		double result = Math.sin(first_number);
+
+		stored_values.push(result);
+
+		return result;
+	}
+
+	private double sinStoredWithHistory()
+	{
+		double history = Double.parseDouble(sb.toString());
+
+		double result = Math.sin(history);
+
+		stored_values.push(result);
+
+		return result;
+
+	}
+
+	private double cosStoredValues()
+	{
+		//System.out.println("wee");
+		double first_number = stored_values.pop();
+		//System.out.println(second_number);
+
+		double result = Math.cos(first_number);
+
+		stored_values.push(result);
+
+		return result;
+	}
+
+	private double cosStoredWithHistory()
+	{
+		double history = Double.parseDouble(sb.toString());
+		double result = Math.cos(history);
+
+		stored_values.push(result);
+
+		return result;
+
+	}
+
+	private double negateStoredValues()
+	{
+		//System.out.println("wee");
+		double first_number = stored_values.pop();
+		//System.out.println(second_number);
+
+		double result = first_number * (-1);
+
+		stored_values.push(result);
+
+		return result;
+	}
+
+	private double negateStoredWithHistory()
+	{
+		double history = Double.parseDouble(sb.toString());
+		double result = history * (-1);
+
+		stored_values.push(result);
+
+		return result;
+
+	}
+	
+	//Changes made here
+	private boolean checkBrackets(String last_entry, String operand)
+	{
+		
+		if(precedence.empty())
+		{
+			return false;
+		}
+		
+		
+		//Check if last_entry was an operation
+		//or a number.
+		try
+		{
+			Double.parseDouble(last_entry);
+		}
+		catch (NumberFormatException e)
+		{
+			//If the two last operands are the same,
+			//no need for brackets
+			if(precedence.peek().equals(operand))
+			{
+				precedence.pop();
+				return false;
+			}
+			//If the last operand is low precedence,
+			//no brackets needed
+			if(lowest_precedence.contains(operand))
+			{
+				
+				return false;
+			}
+			
+			//If both operands are high precedence,
+			//additional checks needed
+			if(highest_precedence.contains(precedence.peek()))
+			{
+				//If multiplication was first,
+				//brackets needed
+				if(operand == DIV && precedence.pop() == MULT)
+					return true;
+				
+				System.out.println("Mult First");
+				//Otherwise, they are not
+				return false;
+				
+			}
+			
+			
+			//Low precedence followed by High.
+			//Thus brackets needed.
+			System.out.println("Brackets");
+			
+			return true;
+		}
+		
+		//Last action involved numbers
+		return false;
+		
+	}
+	
+	private boolean isBinOp()
+	{
+		String last_action = prev_history.peek();
+		if(last_action.equals(ENTER) || last_action.equals(FACT))
+		{
+			return false;
+		}
+		prev_history.pop();
+		return true;
+		
+	}
+	
+	private boolean isSingleOp()
+	{
+		String last_action = prev_history.pop();
+		if(last_action.equals(ENTER))
+		{
+			return false;
+		}
+		return true;
+		
+		
+	}
+	
+	
+	
+}
---
+++ CalcV7/src/View.java
@@ -0,0 +1,334 @@
+import java.awt.GridLayout;
+
+
+
+
+
+
+//import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class View extends JFrame
+{
+	private static final int WINDOW_WIDTH = 600;
+	private static final int WINDOW_HEIGHT = 600;
+	private static final int KEYPAD_WIDTH = 200;
+	private static final int KEYPAD_HEIGHT = 200;
+	private static final int OPERATOR_HEIGHT = 100;
+	private static final int OPERATOR_WIDTH = 100;
+	private static final int DISPLAY_HEIGHT = 300;
+	private static final int DISPLAY_WIDTH = 300;
+	
+	private static final String CLEAR = "Clear";
+	private static final String ENTER = "Enter";
+	private static final String PLUS = "+";
+	private static final String MULT = "x";
+	private static final String DIV = "/";
+	private static final String MINUS = "-";
+	private static final String FACT = "!";
+	//private static final String UNDO = "Undo";
+	private static final String PLUSMINUS = "±";
+	private static final String COS = "COS(";
+	private static final String SIN = "SIN(";
+	
+	private static final String ZERO = "0";
+	private static final String ONE = "1";
+	private static final String TWO = "2";
+	private static final String THREE = "3";
+	private static final String FOUR = "4";
+	private static final String FIVE = "5";
+	private static final String SIX = "6";
+	private static final String SEVEN = "7";
+	private static final String EIGHT = "8";
+	private static final String NINE = "9";
+	
+	private static final String DECIMAL_POINT = ".";
+	
+	
+	
+	private GridLayout window_layout_manager = new GridLayout(3, 1);
+	private GridLayout display_layout_manager = new GridLayout(2, 1);
+	private GridLayout keypad_layout_manager = new GridLayout(4, 4);
+	private GridLayout operator_layout_manager = new GridLayout(5, 1);
+	
+	private JPanel display = new JPanel();
+	private JPanel keypad = new JPanel();
+	private JPanel operators = new JPanel();
+	
+	private JLabel value_display = new JLabel();
+	private JLabel history_display = new JLabel();
+	
+	
+	private Controller controller;
+	
+	
+	public View()
+	{
+		super("Numeric Keypad");
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setLayout(window_layout_manager);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		controller = new Controller(this);
+		
+		keypad.setSize(KEYPAD_WIDTH, KEYPAD_HEIGHT);
+		keypad.setLayout(keypad_layout_manager);
+		keypad.setVisible(true);
+		
+		operators.setSize(OPERATOR_HEIGHT, OPERATOR_WIDTH);
+		operators.setLayout(operator_layout_manager);
+		operators.setVisible(true);
+		
+		display.setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+		display.setLayout(display_layout_manager);
+		display.setVisible(true);
+		
+		
+		add(display);
+		add(keypad);
+		add(operators);
+		display.add(history_display);
+		display.add(value_display);
+		
+		
+		keypad.add(new ButtonAdapter(ONE)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(ONE);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(TWO)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(TWO);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(THREE)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(THREE);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(FOUR)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(FOUR);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(FIVE)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(FIVE);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(SIX)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(SIX);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(SEVEN)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(SEVEN);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(EIGHT)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(EIGHT);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(NINE)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(NINE);
+			}
+		});
+		/*keypad.add(two);
+		keypad.add(three);
+		keypad.add(four);
+		keypad.add(five);
+		keypad.add(six);
+		keypad.add(seven);
+		keypad.add(eight);
+		keypad.add(nine);*/
+		
+		keypad.add(new ButtonAdapter(CLEAR)
+		{
+			public void pressed()
+			{
+				controller.clear();
+			}
+		});
+		//keypad.add(clear);
+		
+		//keypad.add(zero);
+		
+		keypad.add(new ButtonAdapter(ZERO)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(ZERO);
+			}
+		});
+		
+		keypad.add(new ButtonAdapter(ENTER)
+		{
+			public void pressed()
+			{
+				controller.enter();
+			}
+		});
+		
+		//keypad.add(enter);
+		
+		
+		//operators.add(plus);
+		operators.add(new ButtonAdapter(PLUS)
+		{
+			public void pressed()
+			{
+				controller.sum();
+				controller.historyOperation(PLUS);
+			}
+			
+		});
+		
+		
+		//operators.add(minus);
+		operators.add(new ButtonAdapter(MINUS)
+		{
+			public void pressed()
+			{
+				controller.subtract();
+				controller.historyOperation(MINUS);
+			}
+			
+		});
+		
+		//operators.add(multiply);
+		
+		operators.add(new ButtonAdapter(MULT)
+		{
+			public void pressed()
+			{
+				controller.multiply();
+				controller.historyOperation(MULT);
+			}
+			
+		});
+		
+		//operators.add(divide);
+		
+		operators.add(new ButtonAdapter(DIV)
+		{
+			public void pressed()
+			{
+				controller.divide();
+				controller.historyOperation(DIV);
+			}
+			
+		});
+		
+		operators.add(new ButtonAdapter(FACT)
+		{
+			public void pressed()
+			{
+				controller.factorial();
+				controller.singleHistoryOperation(FACT);
+			}
+					
+		});
+		
+		operators.add(new ButtonAdapter(PLUSMINUS)
+		{
+			public void pressed()
+			{
+				controller.negate();
+				controller.singleHistoryOperation(PLUSMINUS);
+			}
+			
+		});
+		
+		operators.add(new ButtonAdapter("COS")
+		{
+			public void pressed()
+			{
+				controller.cos();
+				controller.singleHistoryOperation(COS);
+			}
+			
+		});
+		
+		operators.add(new ButtonAdapter("SIN")
+		{
+			public void pressed()
+			{
+				controller.sin();
+				controller.singleHistoryOperation(SIN);
+			}
+			
+		});
+		
+		//operators.add(decimal_point);
+		operators.add(new ButtonAdapter(DECIMAL_POINT)
+		{
+			public void pressed()
+			{
+				controller.addToEntry(DECIMAL_POINT);
+			}
+		});
+		
+		/*operators.add(new ButtonAdapter(UNDO)
+		{
+			public void pressed()
+			{
+				controller.undo();
+			}
+		});*/
+		
+		value_display.setText("0");
+		history_display.setText("Start new Calculation");
+	}
+	
+	public void updateValue(String next_value)
+	{
+		
+		value_display.setText(next_value);
+	}
+	
+	public void updateHistory(String next_result)
+	{
+		history_display.setText(next_result);
+	}
+	
+	public void clearDisplay()
+	{
+		value_display.setText("0");
+		history_display.setText("Start new Calculation");
+	}
+	
+	
+}
