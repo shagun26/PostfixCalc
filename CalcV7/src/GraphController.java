b72c67078e521226ac4ba7870647708a46773b78
davood96 <david96@my.yorku.ca>
2016-03-30 16:40:50 -0400

    Javadoc

--- CalcV7/src/GraphController.java
+++ CalcV7/src/GraphController.java
@@ -11,21 +11,28 @@ public class GraphController
 	private Stack<String> expressionsList;
 	private String prevExpr;
 	
+	/**
+	 * Initialize the Graphing component of the calculator
+	 * @param controller - the CalcController reference
+	 * @param expressionsPostFix - the Postfix expressions list
+	 * @param expressionsInFix - the Infix expressions list
+	 */
 	public void initialize(Controller controller, Stack<String> expressionsPostFix, Stack<String> expressionsInFix)
 	{
+		//Instantiate according to singleton pattern
 		if(grph_model == null)
 			grph_model = new GraphModel();
 		
 		if(favcntrl == null)
 			favcntrl = new FavController();
 		
-		expressionsList = expressionsInFix;
-		
 		if(grph_view == null)
 			grph_view = new GraphView(this);
 		
+		expressionsList = expressionsInFix;
+	
 		cntrl = controller;
-
+		//If list is empty, reset to default
 		if(expressionsList.empty())
 		{
 			Ys = null;
@@ -33,6 +40,8 @@ public class GraphController
 			grph_view.updateExpr("");
 			prevExpr = "";
 		}
+		//If expression different, get y-values and
+		//draw the graph
 		else if(!expressionsList.peek().equals(prevExpr))
 		{
 			Ys = grph_model.getValues(expressionsPostFix);
@@ -41,31 +50,41 @@ public class GraphController
 			grph_view.drawGraph(Ys, grph_model.getisSCGraph());
 			System.out.println(grph_view.getExpr());
 		}
-			
-			
-
 		grph_view.setVisible(true);
 	}
-	
+	/**
+	 * Instructs the Graph View to open
+	 */
 	public void open()
 	{
 		grph_view.setVisible(true);
 	}
-	
+	/**
+	 * Instructs the Graph View to draw the Graph of the expression
+	 * with the associated y values 
+	 * @param expr - the expression to be graphed
+	 * @param values - the expressions's y coordinates
+	 */
 	public void drawGraph(String expr, double[] values)
 	{
 		grph_view.updateExpr("y = " + expr);
 		grph_view.drawGraph(values, grph_model.getisSCGraph());
 		grph_view.setVisible(true);
 	}
-	
+	/**
+	 * Instructs the Graph View to close and passes control to the
+	 * CalcController (which opens the CalcView)
+	 */
 	public void changeToCalc()
 	{
 		System.out.println("CALC PLS");
 		grph_view.setVisible(false);
 		cntrl.open();
 	}
-
+	/**
+	 * Instructs the Graph View to close and passes control to the
+	 * FavController (which opens the FavcView)
+	 */
 	public void changToFav() 
 	{
 		System.out.println("FAV PLS");
@@ -73,7 +92,10 @@ public class GraphController
 		favcntrl.open(this);
 		
 	}
-
+	/**
+	 * Passes control over to the favorites control
+	 * to save the top expression in the favorites list
+	 */
 	public void addToFav()
 	{
 		if(!expressionsList.empty())
