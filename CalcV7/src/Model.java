d2792a51a2daf9bf05c29f012ec13c969dd2b0e0
dexterfreak <aymanftw@gmail.com>
2016-04-04 12:58:17 -0400

    Reverted changes

--- CalcV7/src/Model.java
+++ CalcV7/src/Model.java
@@ -1219,13 +1219,13 @@ public class Model
 	private boolean checkOpUndo(String first)
 	{
 		if(running_history_undo.empty())
-			return isOp(first);
+			return false;
 		
 		int prev_size = running_history_undo.peek().size();
 		int current_size = running_history.size();
 		if(prev_size >= current_size)
 			return true;
-		return isOp(first);
+		return false;
 	}
 	
 	/**
